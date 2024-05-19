41f80d75897e92b9828d04d4b556fb4e312d605a
Robert <github@ilijason.se>
2024-05-19 10:35:36 +0200

    First version

---
+++ get_page.py
@@ -0,0 +1,88 @@
+import requests
+from bs4 import BeautifulSoup
+from ebooklib import epub
+
+class epub_book_writer():
+    minimum_text_length = 100
+    title = None
+    url = None
+    book = None
+    counter = 1
+    chapter_list = ['nav']
+    table_of_contents = []
+
+
+    def __init__(self, title) -> None:
+        self.title = title
+        self._create_epub_book(title)
+
+
+    # Get the page and remove all the clutter to make it as nice as possible
+    def add_chapter(self, url) -> int:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        for script in soup(['script', 'style', 'header', 'footer', 'nav', 'aside', 'img', 'form', 'noscript']):
+            script.decompose()
+
+        # A mini trick that works frequently enough... Pull the paragraphs and clean them up. Ignore the formatting inside of them.
+        paragraphs = soup.find_all('p')
+        cleaned_paragraphs = []
+
+        for p in paragraphs:
+            cleaned_paragraphs.append(str(p))
+
+        cleaned_text = ''.join(cleaned_paragraphs)
+
+        if len(cleaned_text) <= self.minimum_text_length:
+            return 1
+
+        # Create a chapter
+        page_title = soup.title.string if soup.title else 'Untitled'
+        chapter = epub.EpubHtml(title=page_title, file_name=f'chap_{self.counter}.xhtml', lang='en')
+        source_host = url.split('.')[1]
+        chapter.content = f'<html><head></head><body><h1>From {source_host}</h1>{cleaned_text}</body></html>'
+        self.table_of_contents.append(epub.Link(f'chap_{self.counter}.xhtml', page_title, f'chap_{self.counter}'))
+
+        # Add chapter to the book
+        self.book.add_item(chapter)
+        self.chapter_list.append(chapter)
+        self.counter = self.counter + 1
+
+        return 0
+
+
+    def _create_epub_book(self, title) -> None:
+        # Create the epub book and set some basic metadata
+        self.book = epub.EpubBook()
+
+        # Set metadata
+        self.book.set_title(title)
+        self.book.set_language('en')
+
+
+    def write_epub_book(self) -> None:
+        self.book.spine = self.chapter_list
+        self.book.toc = self.table_of_contents
+
+        # Add default NCX and Nav file
+        self.book.add_item(epub.EpubNcx())
+        self.book.add_item(epub.EpubNav())
+
+        # Define CSS style
+        style = 'body { font-family: Arial, sans-serif; }'
+        nav_css = epub.EpubItem(uid='style_nav', file_name='style/nav.css', media_type='text/css', content=style)
+        self.book.add_item(nav_css)
+
+        # Write to the file
+        epub.write_epub(f'{self.title}.epub', self.book, {})
+
+        print(f'ePub book created successfully: {self.title}.epub')
+
+
+if __name__ == '__main__':
+    e = epub_book_writer('Muppet')
+    e.add_chapter('https://www.vice.com/en/article/m7bxdx/scientists-are-researching-a-device-that-can-induce-lucid-dreams-on-demand')
+    e.add_chapter('https://www.fastcompany.com/91126591/is-the-self-driving-dream-going-to-crash-into-a-regulatory-wall')
+    e.add_chapter('https://www.youtube.com/watch?v=4C7UtIQx3qY')
+    e.write_epub_book()
---
+++ main.py
@@ -0,0 +1,77 @@
+import requests
+import acc
+import get_page
+from datetime import datetime
+
+ACCESS_TOKEN = acc.ACCESS_TOKEN
+SOURCE_COLLECTION_ID = '36864622'
+PROCESSED_COLLECTION_ID = '44271309'
+UNPROCESSED_COLLECTION_ID = '44302142'
+URL = f'https://api.raindrop.io/rest/v1/raindrops/{SOURCE_COLLECTION_ID}/'
+
+
+def get_all_raindrops():
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+        'Content-Type': 'application/json'
+    }
+
+    raindrops = []
+    page = 0
+
+    while True:
+        params = {
+            "page": page
+        }
+        response = requests.get(URL, headers=headers, params=params)
+        # print(response.text)
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to retrieve raindrops: {response.status_code} {response.text}")
+
+        data = response.json()
+        raindrops.extend(data["items"])
+
+        if len(data["items"]) < data["count"]:
+            break
+
+        page += 1
+        break # Temporary solution 
+
+    return raindrops
+
+def update_raindrop(id, no_process):
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'collection': {
+            '$id': UNPROCESSED_COLLECTION_ID if no_process else PROCESSED_COLLECTION_ID
+        }
+    }
+
+    url = f'https://api.raindrop.io/rest/v1/raindrop/{id}'
+    response = requests.put(url, json=data, headers=headers)
+    print(response)
+
+if __name__ == '__main__':
+    todays_date = datetime.now().strftime('%d%m%y')
+    epub = get_page.epub_book_writer(todays_date)
+
+    counter = 1
+    try:
+        raindrops = get_all_raindrops()
+        for raindrop in raindrops:
+            print(f"{raindrop['title']}: {raindrop['link']}")
+            result = epub.add_chapter(raindrop['link'])
+            update_raindrop(raindrop['_id'], result)
+            if counter == 5:
+                break
+            if result == 0:
+                counter = counter + 1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    epub.write_epub_book()
